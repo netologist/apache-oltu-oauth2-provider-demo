07e51dba0663b9a8faec91892d0e0f210e9a19df
Nilath Jayamaha <nilath.jayamaha@auspost.com.au>
2016-11-15 15:56:24 +1100

    Added documentation to the /redirect end point.

--- src/main/java/com/hasanozgan/api/endpoints/RedirectUriEndpoint.java
+++ src/main/java/com/hasanozgan/api/endpoints/RedirectUriEndpoint.java
@@ -10,7 +10,8 @@ import javax.ws.rs.core.Response;
 import java.net.URISyntaxException;
 
 /**
- * This is the client servers redirect end point.  This is created so that we can see Authorisation code that is sent to the client as a part of the redirect url.
+ * This is the client server's redirect end point.  This end point will live in the client's server backend, not in this OAuth service.
+ * This is end point is created so that we can see the Authorisation code that is sent to the client as a part of the redirect url.
  */
 @Path("/redirect")
 public class RedirectUriEndpoint {
